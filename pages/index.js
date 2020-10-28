45878debe4c83c5d8129c681234b48c6f72c2a9e
Ben Martinez-Cain <bmc@bens-air.lan>
2020-10-28 02:04:48 -0400

    add link to repo

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import s from '../components/index.module.css'
 export default function Index({setDark, dark}){
     return(
-        <>
+        <main>
         <div className={s.container}>
             <div className={s.one}/>
             <div className={s.two} />
@@ -14,6 +14,7 @@ export default function Index({setDark, dark}){
             <div className={s.one} />
         </div>
         <button onClick={() => setDark(!dark)}>Toggle dark mode</button>
-        </>
+        <p><a href="https://github.com/TheDormouse/Next-Dark-Mode" targe="_blank">View on GitHub</a></p>
+        </main>
     )
 }
