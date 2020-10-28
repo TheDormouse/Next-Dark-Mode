3cf8ad41073cbef5bd1a0a22a50583e90a6d2e79
Ben Martinez-Cain <bmc@bens-air.lan>
2020-10-28 11:46:19 -0400

    update meta

--- pages/_document.js
+++ pages/_document.js
@@ -14,11 +14,12 @@ class MyDocument extends Document {
             <meta name="description" content="Next.js with Dark Mode - without using libraries"></meta>
             <meta property="og:title" content="Next.js Dark Mode" />
 <meta property="og:description" content="Next.js Dark Mode Demo" />
-<meta property="og:image" content="/demo.gif" />
+<meta property="og:image" content="https://next-dark-mode-five.vercel.app/demo.gif" />
 <meta property="og:url" content="https://next-dark-mode-five.vercel.app/" />
 <meta name="twitter:card" content="summary_large_image" />
 
 <meta property="og:site_name" content="Next.js Dark Mode" />
+<meta property="twitter:image" content="https://next-dark-mode-five.vercel.app/demo.gif" />
 <meta name="twitter:image:alt" content="Next.js Dark Mode" />
 
 <meta name="twitter:site" content="@Mad_Marchy"></meta>
