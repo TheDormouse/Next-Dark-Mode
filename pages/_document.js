e9722b288586c7c8fe20669a0a4503843c69446e
Ben Martinez-Cain <bmc@bens-air.lan>
2020-10-28 11:49:40 -0400

    change to png

--- pages/_document.js
+++ pages/_document.js
@@ -12,14 +12,15 @@ class MyDocument extends Document {
         <Head>
             <title>Next.js Dark Mode</title>
             <meta name="description" content="Next.js with Dark Mode - without using libraries"></meta>
+            <meta name="image" content="https://next-dark-mode-five.vercel.app/demo.png" />
             <meta property="og:title" content="Next.js Dark Mode" />
 <meta property="og:description" content="Next.js Dark Mode Demo" />
-<meta property="og:image" content="https://next-dark-mode-five.vercel.app/demo.gif" />
+<meta property="og:image" content="https://next-dark-mode-five.vercel.app/demo.png" />
 <meta property="og:url" content="https://next-dark-mode-five.vercel.app/" />
 <meta name="twitter:card" content="summary_large_image" />
 
 <meta property="og:site_name" content="Next.js Dark Mode" />
-<meta property="twitter:image" content="https://next-dark-mode-five.vercel.app/demo.gif" />
+<meta property="twitter:image" content="https://next-dark-mode-five.vercel.app/demo.png" />
 <meta name="twitter:image:alt" content="Next.js Dark Mode" />
 
 <meta name="twitter:site" content="@Mad_Marchy"></meta>
